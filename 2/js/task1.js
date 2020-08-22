68b4f0446b0cdcd167691f3c9b48e2def461d93c
Кирилл Гребень <atrakir@gmail.com>
2020-08-22 15:34:07 +0300

    first task done

---
+++ 2/js/task1.js
@@ -0,0 +1,9 @@
+let name = prompt("What is the 'oficial' name of JS?")
+let message;
+
+if (name === "ECMAScript") {
+  message = "That's correct!"
+}
+else message = "You don't know? It is ECMAScript!"
+
+alert(message);
